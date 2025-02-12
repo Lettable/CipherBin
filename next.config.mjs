7984dc6dc58a6f19e90418384715fc76275a8de8
Mirza <devmirzaofficial@gmail.com>
2025-02-12 14:36:57 +0500

    Something!

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,7 @@ const nextConfig = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true,
-  swSrc: '/script/sw.js',
+  skipWaiting: true
 });
 
 export default nextConfig;
