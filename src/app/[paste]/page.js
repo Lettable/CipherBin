ffa8a3998144176d8760bc48ca4d1a5ec5430e2e
Mirza <devmirzaofficial@gmail.com>
2025-02-13 23:18:23 +0500

    fix(gun): add fallback to load paste from local storage if Gun retrieval fails or user's offline

--- src/app/[paste]/page.js
+++ src/app/[paste]/page.js
@@ -48,7 +48,6 @@ export default function PastePage() {
       const gunData = JSON.parse(gunDataStr);
       const pasteKey = `pastes/${uuid}`;
       if (gunData && gunData[pasteKey] && gunData[pasteKey].encoded) {
-        console.log('Paste Obj from LocalStorage', gunData[pasteKey])
         return gunData[pasteKey];
       }
     } catch (e) {
@@ -78,7 +77,6 @@ export default function PastePage() {
           }
           return;
         }
-        console.log('Data', data)
         processData(data);
       });
     }
@@ -86,7 +84,6 @@ export default function PastePage() {
     function processData(data) {
       try {
         const obj = Paste.decodeObject(data.encoded);
-        console.log('OBJ', obj)
         if (new Date(obj.expiresAt) < new Date()) {
           setError("This paste has expired.");
           return;
@@ -106,6 +103,8 @@ export default function PastePage() {
 
     decodePaste();
   }, [pathname]);
+
+
 
   const handleCreatePaste = () => {
     if (!content.trim()) {
